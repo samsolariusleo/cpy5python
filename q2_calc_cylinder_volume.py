92faab846bc70d34c9ea77984139cb8410593992
samsolariusleo <gan.jingying@dhs.sg>
2013-01-22 17:44:32 +0800

    Completed q2.ver1

--- q2_calc_cylinder_volume.py
+++ q2_calc_cylinder_volume.py
@@ -2,4 +2,20 @@
 # Author: Gan Jing Ying
 # Created: 20130122
 # Modified: 20130122
-# Description: 
+# Description: Program that computes the volume of a cylinder.
+
+# main
+
+# prompt user to input radius of circular base
+radius = int(input("Enter radius: "))
+
+# prompt user to input length of cylinder
+length = int(input("Enter length: ")
+
+# calculate volume of cylinder
+from math import *
+area = radius * radius * pi
+volume = area * length
+
+# return volume
+print(volume)
