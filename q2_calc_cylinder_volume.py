bcf1bbabc588f4138c51f67b078657801f41aebd
Sam Gan <gan.jingying@dhs.sg>
2013-01-22 18:15:38 +0800

    Added comments for structure

--- q2_calc_cylinder_volume.py
+++ q2_calc_cylinder_volume.py
@@ -4,11 +4,18 @@
 # Modified: 20130122
 # Description: Program that calculates the volume of a cylinder.
 
+# main
+
+# prompt for radius
 radius = int(input("Enter radius: "))
+
+# prompt for length
 length = int(input("Enter length: "))
 
+# calculate volume
 from math import pi
 area = radius * radius * pi
 volume = area * length
 
+# return value of volume
 print("Volume: {0:.3f}".format(volume))
