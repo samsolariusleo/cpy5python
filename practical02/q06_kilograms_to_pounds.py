fbddb3e1eb7d531fff63afc3dea0b478cd83b140
samsolariusleo <gan.jingying@dhs.sg>
2013-02-07 12:00:00 +0800

    Completed q06

--- practical02/q06_kilograms_to_pounds.py
+++ practical02/q06_kilograms_to_pounds.py
@@ -5,3 +5,11 @@
 # Description: Program that converts kilograms to pounds and creates a table.
 
 # main
+
+# print headers for table
+print("{0:10s}".format("Kilograms") + "{0:7s}".format("Pounds"))
+
+# create for loop and print results
+for kilograms in range(1,11):
+    pounds = kilograms * 2.2
+    print("{0:<10}".format(kilograms) + "{0:<7.1f}".format(pounds))
