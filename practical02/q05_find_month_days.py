3729a22f6a62252c67a8869e28416207318e7cf0
samsolariusleo <gan.jingying@dhs.sg>
2013-02-07 11:27:13 +0800

    Fixed errors with int and str, completed q05

--- practical02/q05_find_month_days.py
+++ practical02/q05_find_month_days.py
@@ -18,8 +18,8 @@ allmonths = ["January", "February", "March", "April", "May", "June",
               "July", "August", "September", "October", "November", "December"]
 
 # define list of days
-alldays = [31, 28, 31, 30, 31, 30,
-           31, 31, 30, 31, 30, 31, 29]
+alldays = ["31", "28", "31", "30", "31", "30",
+           "31", "31", "30", "31", "30", "31", "29"]
 
 # check if month is feb
 # if month is feb, then check if year is leap year
@@ -27,6 +27,8 @@ alldays = [31, 28, 31, 30, 31, 30,
 # else, days are normal
 
 if month == 2 and year % 4 == 0:
-    print(allmonths[month-1] + year + "has" + alldays[-1] + "days")
+    print(allmonths[month-1] + " " + str(year) + " has " + alldays[-1] +
+          " days")
 else:
-    print(allmonths[month-1] + year + "has" + alldays[month-1] + "days")
+    print(allmonths[month-1] + " " + str(year) + " has " + alldays[month-1] +
+          " days")
