01a897cc630f73850001d419be78f71bf7e6467d
samsolariusleo <gan.jingying@dhs.sg>
2013-02-06 19:34:58 -0800

    Create q06_kilograms_to_pounds.py

---
+++ practical02/q06_kilograms_to_pounds.py
@@ -0,0 +1,7 @@
+# Filename: q06_kilograms_to_pounds.py
+# Author: Gan Jing Ying
+# Created: 20130207
+# Modified: 20130207
+# Description: Program that converts kilograms to pounds and creates a table.
+
+# main
