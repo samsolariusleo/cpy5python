2bcf2bdd83af4983d30ad9641fffa58f6600963f
Sam Gan <gan.jingying@dhs.sg>
2013-02-19 19:24:33 +0800

    Created and completed q01

---
+++ practical03/q01_display_reverse.py
@@ -0,0 +1,17 @@
+# Filename: q1_display_reverse.py
+# Author: Gan Jing Ying
+# Created: 20130219
+# Modified: 20130219
+
+# define variables
+i = 0
+numbers = []
+
+# define a procedure
+def reverse_int(n):
+    return str(n)[::-1]
+
+# main
+integer = input("Enter integer: ")
+print("Reverse = " + reverse_int(integer))
+
