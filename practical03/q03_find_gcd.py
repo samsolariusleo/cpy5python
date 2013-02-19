8b57eaf9f3f207aa7b86728eb90eff844846322d
Sam Gan <gan.jingying@dhs.sg>
2013-02-19 20:12:34 +0800

    Refined and completed q03

--- practical03/q03_find_gcd.py
+++ practical03/q03_find_gcd.py
@@ -8,25 +8,23 @@
 # define variables
 
 # define function
-def smaller(m,n):
+def smaller(m, n):
     if m > n:
         smaller_no = n
     else:
         smaller_no = m
     return m,n,smaller_no
     
-def find_gcd(m,n,d):
+def find_gcd(m, n, d):
     while m % d != 0 or n % d != 0:
         d = d - 1
     return d
 
-def gcd(m,n):
+def gcd(m, n):
     m,n,d = smaller(m,n)
     greatest = find_gcd(m,n,d)
     return greatest
     
 # main
-integer01 = int(input("Enter first integer: "))
-integer02 = int(input("Enter second integer: "))
-
-print("The Greatest Common Divisor is " + str(gcd(integer01,integer02)))
+print(gcd(24, 16))
+print(gcd(255, 25))
