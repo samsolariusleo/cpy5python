7898de798709542449032c1784fc3fcd1d63836a
Sam Gan <gan.jingying@dhs.sg>
2013-02-25 17:05:14 +0800

    Created new folder and added q01

---
+++ practical04/q01_sum_series.py
@@ -0,0 +1,17 @@
+# Filename: q01_sum_series.py
+# Author: Gan Jing Ying
+# Created: 20130225
+# Modified: 20130225
+# Description: Program that defines a recursive function that computes the sum
+# of a series of numbers.
+
+# define recursive function
+def sum_series(i):
+    if i == 0:
+        return 0
+    elif i == 1:
+        return 1
+    else:
+        return sum_series(
+
+# main
