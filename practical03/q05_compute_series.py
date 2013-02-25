90206c92eafb6621eca742255d57e5b3137920d0
Sam Gan <gan.jingying@dhs.sg>
2013-02-25 17:02:56 +0800

    Created and completed q05

--- practical03/q05_compute_series.py
+++ practical03/q05_compute_series.py
@@ -8,5 +8,16 @@
 # define variables
 
 # define functions
+def m_series(i):
+    sum_of_no = 0 # initialize the sum of numbers
+    for e in range(1,i+1,2):
+        sum_of_no = sum_of_no + 1/((2 * e) - 1) - 1/((2 * e + 1))
+    sum_of_no = sum_of_no * 4
+    return sum_of_no
+
+def print_series(i):
+    for e in range(1,i+1):
+        print("{0:<5}".format(e) + "{0:<15.11f}".format(m_series(e)))
 
 # main
+print_series(19)
