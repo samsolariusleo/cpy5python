2385dff7631fffdcd3d4300e1c1f370f2fd8e4db
Sam Gan <gan.jingying@dhs.sg>
2013-02-25 15:49:08 +0800

    Created and completed q04

--- practical03/q04_sum_series.py
+++ practical03/q04_sum_series.py
@@ -8,5 +8,15 @@
 # define variables
 
 # define functions
+def m_series(i):
+    sum_of_no = 0
+    for e in range(1,i+1):
+        sum_of_no = sum_of_no + (e/(e+1))
+    return sum_of_no
+
+def print_table(i):
+    for e in range(1,i+1):
+        print("{0:<10}".format(e) + "{0:<10.4f}".format(m_series(e)))
 
 # main
+print_table(20)
